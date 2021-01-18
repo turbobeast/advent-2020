e5128268419fb120e0c496ebfc6de3c1eb008455
turbobeast <georgemichaelfaust@gmail.com>
2021-01-17 20:47:33 -0500

    reefactor

--- adventutils/one.go
+++ adventutils/one.go
@@ -1,29 +1,52 @@
 package adventutils
 
-func OneA(invoices []int) int {
-	for indexA, a := range invoices {
-		matches := invoices[indexA+1 : len(invoices)]
-		for _, b := range matches {
-			if a+b == 2020 {
-				return a * b
+import (
+	"errors"
+	"os"
+)
+
+func findTwoTermsFor(sum int, terms []int) (a, b int, err error) {
+	for indexA, a := range terms {
+		termsSlice := terms[indexA+1 : len(terms)]
+		for _, b := range termsSlice {
+			if a+b == sum {
+				return a, b, nil
 			}
 		}
 	}
-	return 0
+	return 0, 0, errors.New("Could not find numbers")
 }
 
-func OneB(invoices []int) int {
-	for indexA, a := range invoices {
-		firstMatches := invoices[indexA+1 : len(invoices)]
-		for indexB, b := range firstMatches {
-			secondMatches := firstMatches[indexB+1 : len(firstMatches)]
-			for _, c := range secondMatches {
-				if a+b+c == 2020 {
-					return a * b * c
+func findThreeTermsFor(sum int, terms []int) (a, b, c int, err error) {
+	for indexA, a := range terms {
+		termsSliceA := terms[indexA+1 : len(terms)]
+		for indexB, b := range termsSliceA {
+			termsSliceB := termsSliceA[indexB+1 : len(termsSliceA)]
+			for _, c := range termsSliceB {
+				if a+b+c == sum {
+					return a, b, c, nil
 				}
 			}
 		}
 	}
 
-	return 0
+	return 0, 0, 0, errors.New("Could not find numbers")
+}
+
+func OneA(invoices []int) int {
+	a, b, err := findTwoTermsFor(2020, invoices)
+	if errors.Is(err, os.ErrExist) {
+		return 0
+	}
+
+	return a * b
+}
+
+func OneB(invoices []int) int {
+	a, b, c, err := findThreeTermsFor(2020, invoices)
+	if errors.Is(err, os.ErrExist) {
+		return 0
+	}
+
+	return a * b * c
 }
